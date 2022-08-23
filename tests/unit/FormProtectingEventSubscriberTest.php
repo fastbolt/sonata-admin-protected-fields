4477084154296674803494dd3be9d028da087bd9
Daniel Hirtzbruch <dhh@fastbolt.com>
2022-08-23 11:04:14 +0200

    Delete protection working

---
+++ tests/unit/FormProtectingEventSubscriberTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Fastbolt\SonataAdminProtectedFields\Tests\Unit\EventSubscriber;
+
+use Fastbolt\SonataAdminProtectedFields\EventSubscriber\FormProtectingEventSubscriber;
+use PHPUnit\Framework\TestCase;
+
+class FormProtectingEventSubscriberTest extends TestCase
+{
+
+}
