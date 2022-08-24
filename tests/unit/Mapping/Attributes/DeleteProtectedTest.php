389b42c37f9a7d8131a31bbc6c7e13c61a5ab4cf
Daniel Hirtzbruch <dhh@fastbolt.com>
2022-08-24 08:40:43 +0200

    Workflows

--- tests/unit/Mapping/Attributes/DeleteProtectedTest.php
+++ tests/unit/Mapping/Attributes/DeleteProtectedTest.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Copyright © Fastbolt Schraubengroßhandels GmbH.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Fastbolt\SonataAdminProtectedFields\Tests\Unit\Mapping\Attributes;
 
 use Fastbolt\SonataAdminProtectedFields\Mapping\Attributes\DeleteProtected;
--- tests/unit/_Fixtures/Mapping/ClassWithProtectedFieldsAndDeleteProtection.php
+++ tests/unit/_Fixtures/Mapping/ClassWithProtectedFieldsAndDeleteProtection.php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * @noinspection PhpUndefinedClassInspection
- * @noinspection PhpPropertyOnlyWrittenInspection
+ * Copyright © Fastbolt Schraubengroßhandels GmbH.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Fastbolt\SonataAdminProtectedFields\Tests\Unit\_Fixtures\Mapping;
